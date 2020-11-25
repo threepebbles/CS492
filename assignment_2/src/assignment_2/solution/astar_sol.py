82bf2e440b89f362101c455330a1eea1e6b7c387
heiler <threepebbles@naver.com>
2020-11-25 20:21:20 +0900

    assignment 2 solution

---
+++ assignment_2/src/assignment_2/solution/astar_sol.py
@@ -0,0 +1,240 @@
+import math
+import numpy as np
+
+class Node:
+    """
+    Node class for dijkstra search
+    """
+    def __init__(self, pos, idx, cost, h, prev_idx):
+        self.pos  = np.array(pos)
+        self.idx  = idx
+        self.cost = cost
+        self.h    = h
+        self.prev_idx = prev_idx # previous node's index
+
+    def __str__(self):
+        return str(self.x) + "," + str(self.cost) + "," + str(self.prev_idx)
+
+
+def get_grid_index(state, resolution, limits, grid_dim):
+    """ 
+    A function to convert a state to the index of grid.
+
+    Parameters
+    ----------
+    state : list
+        a list of coordinate values
+    resolution : float
+        a value of grid resolution
+    grid_limits : list
+        a list of minimum and maximum configuration limits, where
+        each limit is a list of float values.
+        (e.g., [[min_x,_min_y,min_z],[max_x,max_y,max_z]])
+    grid_dim : list
+        a list of grid dimensions.
+
+    Returns
+    -------
+     : Integer
+        The index of the input state in grid.
+    """
+    if type(state) in [list, tuple]: state = np.array(state)
+        
+    ids = np.round((state-limits[0])/resolution).astype(int)
+    if len(state)==2:
+        idx = ids[0]*grid_dim[1]+ids[1]
+    elif len(state)==3:
+        idx = ids[0]*grid_dim[1]*grid_dim[2]+ids[1]*grid_dim[2]+ids[2]
+    else:
+        return NotImplemented
+    
+    if idx<0 or idx>=grid_dim[0]*grid_dim[1]*grid_dim[2]:
+        print("out of grid: {}".format(idx) )
+    return idx
+
+
+def get_grid_pos(state, resolution, limits):
+    """ 
+    A function that return the nearest coordinate on the grid.
+
+    Parameters
+    ----------
+    state : list
+        a list of coordinate values
+    resolution : float
+        a value of grid resolution
+    grid_limits : list
+        a list of minimum and maximum configuration limits, where
+        each limit is a list of float values.
+        (e.g., [[min_x,_min_y,min_z],[max_x,max_y,max_z]])
+
+    Returns
+    -------
+    new_state : list
+        a list of the nearest coordinate values on the grid
+    """
+    if type(state) in [list, tuple]: state = np.array(state)
+        
+    ids = np.round((state-limits[0])/resolution).astype(int)
+
+    new_state = limits[0] + resolution*ids
+    return new_state.tolist()
+
+
+def is_valid(state, grid_limits, obstacle_tree, robot_size):
+    """ 
+    A function to check the validity of the input state.
+
+    Parameters
+    ----------
+    state : list
+        a list of coordinate values
+    grid_limits : list
+        a list of minimum and maximum configuration limits, where
+        each limit is a list of float values.
+        (e.g., [[min_x,_min_y,min_z],[max_x,max_y,max_z]])
+    obstacle_tree : BallTree 
+        a BallTree object from Scikit-Learn. The tree includes
+        the coordinates of obstacles.
+    robot_size : float
+        a radius of a robot. This value is used for collision checking.       
+
+    Returns
+    -------
+     : boolean
+        True if the input state is valid
+    """
+    ## from IPython import embed; embed(); sys.exit()
+    
+    # check workspace limits
+    if any( state[i] < grid_limits[0][i] for i in range(len(grid_limits[0])) ):
+        return False
+    if any( state[i] > grid_limits[1][i] for i in range(len(grid_limits[1])) ):
+        return False
+
+    # check collision
+    if len(np.shape(state))==1: state=state[np.newaxis,:]
+    count = obstacle_tree.query_radius(state, robot_size, count_only=True)
+    if count>0: return False
+    return True
+    
+    
+def astar_planning(start, goal, actions, resolution, grid_limits,
+                       obstacle_tree, robot_size, **kwargs):
+    """ 
+    A function to generate a path from a start to a goal 
+    using A* search-based planning algorithm.  
+
+    Parameters
+    ----------
+    start : list
+        a list of start coordinate values (e.g., [x,y,z]).  
+    goal : list
+        a list of goal coordinate values (e.g., [x,y,z]).  
+    actions : list
+        a list of available actions, where each action is a list
+        of float values (e.g., [[vx,vy,vz],[vx,vy,vz], ..., ]).  
+    resolution : float
+        a value of grid resolution
+    grid_limits : list
+        a list of minimum and maximum configuration limits, where
+        each limit is a list of float values.
+        (e.g., [[min_x,_min_y,min_z],[max_x,max_y,max_z]])
+    obstacle_tree : BallTree 
+        a BallTree object from Scikit-Learn. The tree includes
+        the coordinates of obstacles.
+    robot_size : float
+        a radius of a robot. This value is used for collision checking.       
+
+    Returns
+    -------
+    path : list
+        a list of coordinate values that is the shortest path from 
+        the start to the goal.
+    """
+    if type(grid_limits) is not np.ndarray: grid_limits = np.array(grid_limits)
+    grid_dim    = np.round((grid_limits[1]-grid_limits[0])/resolution+1).astype(int)
+    
+    openset, closedset = dict(), dict()
+
+    # Set a start node
+    start_node = Node(start,
+                      get_grid_index(start, resolution, grid_limits, grid_dim),
+                      0, np.linalg.norm(np.array(start)-goal), -1)
+    openset[start_node.idx] = start_node
+
+    # Set a goal node
+    goal_node = Node(goal,
+                     get_grid_index(goal, resolution, grid_limits, grid_dim),
+                     0, 0, -1)
+    if start_node.idx==goal_node.idx: return []
+    print("Start and goal indices: {} and {}".format(start_node.idx,
+                                                         goal_node.idx))
+
+    
+    while True:
+        # Empty openset
+        if len(openset) == 0: return None
+
+        #------------------------------------------------------------
+        # ADD YOUR CODE
+        #------------------------------------------------------------
+        cur_idx  = min(openset, key=lambda o: openset[o].cost+openset[o].h)
+        cur_node = openset[cur_idx]
+
+        if cur_idx == goal_node.idx :
+            print("goal found! {}, {}".format(cur_idx, cur_node.prev_idx))
+            goal_node.prev_idx = cur_node.prev_idx
+            goal_node.cost = cur_node.cost
+            goal_node.h    = cur_node.h
+            break
+        #------------------------------------------------------------
+
+        # Remove the item from the open set
+        del openset[cur_idx]
+
+        #------------------------------------------------------------
+        # ADD YOUR CODE
+        #------------------------------------------------------------        
+        # Add it to the closed set
+        closedset[cur_idx] = cur_node
+
+        # expand nodes based on available actions
+        for i, action in enumerate(actions): 
+            next_pos = cur_node.pos+action
+            if is_valid(next_pos, grid_limits, obstacle_tree, robot_size) is False:
+                continue
+            
+            dist1 = np.linalg.norm(next_pos - cur_node.pos)
+            dist2 = np.linalg.norm(next_pos - goal)
+            
+            node = Node(next_pos,
+                        get_grid_index(next_pos, resolution, grid_limits, grid_dim),
+                        cur_node.cost + dist1,
+                        dist2,
+                        cur_idx)
+            if node.idx in closedset: continue
+            
+            # Otherwise if it is already in the open set
+            if node.idx in openset:
+                if openset[node.idx].cost > node.cost:
+                    openset[node.idx] = node
+            else:
+                openset[node.idx] = node
+        #------------------------------------------------------------
+
+    # Track the path from goal to start
+    path = [goal_node.pos]    
+    #------------------------------------------------------------
+    # ADD YOUR CODE
+    #------------------------------------------------------------
+    prev_idx = goal_node.prev_idx
+    while prev_idx != start_node.idx:
+        n = closedset[prev_idx]
+        path.append(n.pos)
+        prev_idx = n.prev_idx
+    return path[::-1]
+    #------------------------------------------------------------
+
+                
+
---
+++ assignment_2/src/assignment_2/solution/move_astar_sol.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+import sys, time
+import rospy
+import numpy as np, math
+import PyKDL
+
+from geometry_msgs.msg import PoseStamped, Point, Quaternion, PoseArray, Pose
+from assignment_1 import misc
+import quaternion
+
+from sklearn.neighbors import BallTree
+from manip_challenge import add_object as ao
+
+#import move_joint as mj
+import move_joint_sol as mj
+#import astar
+import astar_sol as astar
+
+
+def generate_obstacles(resolution=0.05):
+    """ 
+    A function to generate obstacles
+
+    Parameters
+    ----------
+    resolution : float
+        a value of grid resolution
+
+    Returns
+    -------
+    obstacles : list
+        a list of obstacle coordinates.
+    obstacle_tree : BallTree 
+        a BallTree object from Scikit-Learn. The tree includes
+        the coordinates of obstacles.
+    """
+    X,Y,Z = np.meshgrid(np.arange(0.3,0.8,resolution).astype(float),
+                        np.arange(0.,0.1,resolution).astype(float),
+                        np.arange(0,0.25,resolution).astype(float))
+    obstacles = np.vstack([np.ravel(X),np.ravel(Y),np.ravel(Z)]).T
+    obstacle_tree = BallTree(obstacles)
+    
+    return obstacles, obstacle_tree
+
+    
+def test(arm):
+    """ 
+    A function to generate obstacle avoidance motion
+
+    Parameters
+    ----------
+    arm : class object
+        a class object to control the robot
+
+    """
+
+    # initialize variables
+    resolution  = 0.025
+    robot_size = 0.1    
+    actions = [[-1,-1,-1], [-1,0,-1], [-1,1,-1], [0,-1,-1],
+               [0,0,-1], [0,1,-1], [1,-1,-1], [1,0,-1], [1,1,-1],
+               [-1,-1,0], [-1,0,0], [-1,1,0], [0,-1,0],
+               [0,1,0], [1,-1,0], [1,0,0], [1,1,0],
+               [-1,-1,1], [-1,0,1], [-1,1,1], [0,-1,1], [0,0,1],
+               [0,1,1], [1,-1,1], [1,0,1], [1,1,1] ]
+    actions = np.array(actions)*resolution
+    grid_limits = [[0.4,-0.4,0.0],[0.7,0.3,0.5]]
+    obstacles, obstacle_tree = generate_obstacles()
+    
+    # initialize the robot
+    arm.move_joint([0.1646, -1.3117, 1.9484, -2.2074, -1.5707, 0.1646])
+    rospy.sleep(3)
+
+    # get the start pose
+    start_pose = arm.fk_request(arm.js_joint_position)
+
+    # set the goal pose
+    goal_pose = Pose()
+    goal_pose.position.x = 0.52
+    goal_pose.position.y = -0.2
+    goal_pose.position.z = 0.1677 - 0.15
+    goal_pose.orientation.x = 0.707
+    goal_pose.orientation.y = 0.707
+    goal_pose.orientation.z = 0.0
+    goal_pose.orientation.w = 0.0
+
+    start = [start_pose.position.x,start_pose.position.y,start_pose.position.z] 
+    goal  = [goal_pose.position.x,goal_pose.position.y,goal_pose.position.z]
+
+    # get the nearest pose on the grid before planning
+    start_on_grid = astar.get_grid_pos(start, resolution,
+                                          grid_limits )
+    goal_on_grid = astar.get_grid_pos(goal, resolution,
+                                          grid_limits )
+
+    # run the planning algorithm
+    path = astar.astar_planning(start_on_grid, goal_on_grid, actions,
+                                resolution, grid_limits,
+                                obstacle_tree, robot_size)
+    pos_traj = [np.array(start)] + path + [np.array(goal)]
+
+    
+    # smoothing if you can
+    # ...
+    
+    pose_traj = []
+    for i, pos in enumerate(pos_traj):
+        p = Pose()
+        # ------------------------------------------------------
+        # Place your code here
+        # ------------------------------------------------------
+        # position
+        p.position.x = pos[0]
+        p.position.y = pos[1]
+        p.position.z = pos[2]
+        p.orientation = quaternion.slerp(start_pose.orientation,
+                                         goal_pose.orientation,
+                                         float(i)/(len(pos_traj)-1))
+        # ------------------------------------------------------
+        pose_traj.append(p)
+    
+    arm.move_pose_trajectory(pose_traj, duration=5.)
+
+    
+        
+if __name__ == '__main__':
+    rospy.init_node("test_move", anonymous=True, disable_signals=True)
+    rospy.sleep(1)
+
+    # create action client
+    arm = mj.ArmClient()    
+    test(arm)
+    
+
